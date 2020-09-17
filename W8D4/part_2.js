6755d4caa47bfcd04543b272f7e5ceb301df8722
AlexArchibeque <wonkman33@gmail.com>
2020-09-16 22:04:47 -0700

    Still a bit stuck

---
+++ W8D4/part_2.js
@@ -0,0 +1,18 @@
+function titleize(names, cb){
+    let arr = [];
+
+    names.prototype.map(); {
+
+    }
+
+}
+
+function printCallback(arr) {
+
+}
+
+titleize(["Mary", "Brian", "Leo"], printCallback);
+// Mx. Mary Jingleheimer Schmidt
+// Mx. Brian Jingleheimer Schmidt
+// Mx. Leo Jingleheimer Schmidt
+// undefined
