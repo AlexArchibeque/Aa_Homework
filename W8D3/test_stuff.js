be09e7b787cf0bbe410285d67fff375662ae2fdd
AlexArchibeque <wonkman33@gmail.com>
2020-09-19 13:05:16 -0700

    solutions and things

--- W8D3/test_stuff.js
+++ W8D3/test_stuff.js
@@ -50,5 +50,67 @@ function madLib(...args) {
 }
 
 
-madLib('make', 'best', 'guac');
+// madLib('make', 'best', 'guac');
 //"We shall MAKE the BEST GUAC."
+
+function isSubstring(str1,str2){
+
+  arr = str1.split(' ');
+
+  let ans;
+
+  arr.forEach(el => {
+    debugger
+    if (el === str2){
+      ans = true   
+    }
+  });
+
+  if(ans){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+// function isSubstring(str1,str2){
+//   return str1.includes(str2);
+// }
+
+// console.log(isSubstring("time to program", "time"))
+//true
+// console.log(isSubstring('Thats not a meatball', 'Spagheetti'))
+
+// console.log(isSubstring("Jump for joy", "joys"))
+//false
+
+
+
+
+function isPrime(num) {
+  if(num < 2){
+    console.log(false);
+    return false;
+  }
+
+  for (let i=2; i < num ; i++) {
+    if(num % i === 0){
+      console.log(false);
+      return false;
+    }
+  }
+  console.log(true);
+  return true;
+}
+
+// isPrime(2)
+// // true
+
+// isPrime(10)
+// // false
+
+// isPrime(15485863)
+// // true
+
+// isPrime(3548563)
+// // false
