254990ada09c4f8a32313a322b2ef8c21f47f055
AlexArchibeque <wonkman33@gmail.com>
2020-09-19 00:32:39 -0700

    Madlib interpolated

--- W8D3/test_stuff.js
+++ W8D3/test_stuff.js
@@ -1,45 +1,54 @@
-function mysteryScoping1() {
-    var x = 'out of block';
-    if (true) {
-      var x = 'in block';  
-      console.log(x);
-    }
-    console.log(x);
-  }
+// function mysteryScoping1() {
+//     var x = 'out of block';
+//     if (true) {
+//       var x = 'in block';  
+//       console.log(x);
+//     }
+//     console.log(x);
+//   }
   
-  function mysteryScoping2() {
-    const x = 'out of block';
-    if (true) {
-      const x = 'in block';  
-      console.log(x);
-    }
-    console.log(x);
-  }
+//   function mysteryScoping2() {
+//     const x = 'out of block';
+//     if (true) {
+//       const x = 'in block';  
+//       console.log(x);
+//     }
+//     console.log(x);
+//   }
   
-  function mysteryScoping3() {
-    const x = 'out of block';
-    if (true) {
-      var x = 'in block';  
-      console.log(x);
-    }
-    console.log(x);
-  }
+//   function mysteryScoping3() {
+//     const x = 'out of block';
+//     if (true) {
+//       var x = 'in block';  
+//       console.log(x);
+//     }
+//     console.log(x);
+//   }
   
-  function mysteryScoping4() {
-    let x = 'out of block';
-    if (true) {
-      let x = 'in block';  
-      console.log(x);
-    }
-    console.log(x);
-  }
+//   function mysteryScoping4() {
+//     let x = 'out of block';
+//     if (true) {
+//       let x = 'in block';  
+//       console.log(x);
+//     }
+//     console.log(x);
+//   }
   
-  function mysteryScoping5() {
-    let x = 'out of block';
-    if (true) {
-      let x = 'in block';  
-      console.log(x);
-    }
-    let x = 'out of block again';
-    console.log(x);
-  }
+//   function mysteryScoping5() {
+//     let x = 'out of block';
+//     if (true) {
+//       let x = 'in block';  
+//       console.log(x);
+//     }
+//     let x = 'out of block again';
+//     console.log(x);
+//   }
+
+function madLib(...args) {
+
+  console.log(`We shall ${args[0].toUpperCase()} the ${args[1].toUpperCase()} ${args[2].toUpperCase()}. `)
+}
+
+
+madLib('make', 'best', 'guac');
+//"We shall MAKE the BEST GUAC."
