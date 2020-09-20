bb351246a529142371de3a379fe309dc920e5f5e
AlexArchibeque <wonkman33@gmail.com>
2020-09-20 08:25:22 -0700

    Some more things

--- W8D4/part_2.js
+++ W8D4/part_2.js
@@ -1,14 +1,12 @@
 function titleize(names, cb){
-    let arr = [];
-
-    names.prototype.map(); {
-
-    }
-
+    let arr = names.map(el =>`Mx. ${el} Jingleheimer Schmidt`);
+    cb(arr);
 }
 
 function printCallback(arr) {
-
+    arr.forEach(el => {
+        console.log(el)
+    })
 }
 
 titleize(["Mary", "Brian", "Leo"], printCallback);
